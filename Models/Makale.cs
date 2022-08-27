e27741abbe5c3e203cebe404d96445b2fedadff5
RabiaZengin <62606076+RabiaZengin@users.noreply.github.com>
2022-08-27 23:01:55 +0300

    Add files via upload

---
+++ Models/Makale.cs
@@ -0,0 +1,48 @@
+namespace WebProgramlamaProje.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Makale")]
+    public partial class Makale
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Makale()
+        {
+            Yorums = new HashSet<Yorum>();
+            Etikets = new HashSet<Etiket>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Başlık")]
+        public string Baslık { get; set; }
+
+        [Required]
+        [Display(Name ="İçerik")]
+        public string Icerik { get; set; }
+
+        public int KullaniciId { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Tarih { get; set; }
+
+        public int KategoriId { get; set; }
+
+        public virtual Kategori Kategori { get; set; }
+
+        public virtual Kullanici Kullanici { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Yorum> Yorums { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Etiket> Etikets { get; set; }
+    }
+}
---
+++ ViewModels/SonAtilanMakaleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebProgramlamaProje.Models;
+
+namespace WebProgramlamaProje.ViewModels
+{
+    public class SonAtilanMakaleViewModel
+    {
+        public Makale Makalem { get; set; }
+
+        public List<Makale> SonMakaleler { get; set; }
+    }
+}
