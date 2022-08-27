05468b170d62f6b5ebc80ccc903ac5a629736c41
RabiaZengin <62606076+RabiaZengin@users.noreply.github.com>
2022-08-27 22:59:15 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProgramlamaProje.Models;
+
+namespace WebProgramlamaProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        BlogDB db = new BlogDB();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Login(Kullanici model)
+        {
+            try
+            {
+                var varmi = db.Kullanicis.Where(i => i.KullaniciAdi == model.KullaniciAdi).SingleOrDefault();
+
+                if (varmi == null)
+                {
+                    return View();
+                }
+                if (varmi.Sifre == model.Sifre)
+                {
+                    Session["username"] = varmi.KullaniciAdi;
+                    return RedirectToAction("Index","Kullanici");
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            catch
+            {
+
+                return View();
+            }
+
+        }
+
+
+        // GET: Kullanici/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Kullanici/Create
+        [HttpPost]
+        public ActionResult Create(Kullanici model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var varmi = db.Kullanicis.Where(i => i.KullaniciAdi == model.KullaniciAdi).SingleOrDefault();
+
+                if (varmi != null)
+                {
+                    return View();
+                }
+
+                if (string.IsNullOrEmpty(model.Sifre))
+                {
+                    return View();
+                }
+                //model.KayıtTarihi = DateTime.Now;
+                model.YetkiId = 1;
+                db.Kullanicis.Add(model);
+                db.SaveChanges();
+                Session["username"] = model.KullaniciAdi;
+                return RedirectToAction("Index","Kullanici");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
